25c4ee54f136a86f178bcff1d0403861d4a2a282
Matthew Keller <matthewjameskeller@outlook.com>
2024-10-16 14:40:21 -0400

    js finished

--- back.py
+++ back.py
@@ -1,12 +1,25 @@
-import json,jsonify
-from flask import Flask
-
+import json
+from flask import Flask,jsonify,request
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/retrieve")
-function retrieve():
+def retrieve():
     with open ('top_tracks_2023.json') as f:
         tracks = json.load(f)
         return jsonify(tracks)
+
+@app.route("/search", methods = ['POST'])
+def searchProcess():
+    req = request.json
+    filterType = req["select"]
+    offensive = req["explicit"]
+    sortBy = req["sortInput"]
+    searchIn = req["searchInput"]
+    
+    return jsonify(searchResult)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
         
