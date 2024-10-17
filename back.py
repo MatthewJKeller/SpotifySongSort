d22beccd606a6ca7ef379eeba5498e865664297f
olongsworth <longol4@furman.edu>
2024-10-16 20:49:33 -0400

    Add files via upload

--- back.py
+++ back.py
@@ -1,25 +1,39 @@
 import json
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-
+ 
+@app.route('/')
+def home():
+    return render_template('task.html')
+ 
 @app.route("/retrieve")
 def retrieve():
     with open ('top_tracks_2023.json') as f:
         tracks = json.load(f)
         return jsonify(tracks)
-
+ 
 @app.route("/search", methods = ['POST'])
 def searchProcess():
     req = request.json
     filterType = req["select"]
-    offensive = req["explicit"]
-    sortBy = req["sortInput"]
-    searchIn = req["searchInput"]
-    
-    return jsonify(searchResult)
-    
+    explicit = req["explicit"]
+    sortBy = req["sortBy"]
+    searchQuery = req["searchQuery"].lower()
+ 
+    with open ('top_tracks_2023.json') as f:
+        tracks = json.load(f)
+ 
+    #Filtering title - string search
+    if filterType == "title":
+        searchResults = [title for title in tracks if searchQuery in title[filterType].lower()]
+        return jsonify(searchResults)
+   
+    #Filtering genres and artists requires filtering a list
+    if filterType in ("genres", "artists"):
+        searchResults = [title for title in tracks if searchQuery in title[filterType].lower()]
+        return jsonify(searchResults)
+ 
 if __name__ == "__main__":
     app.run(debug=True)
-        
